eea150459f5c6be63affabb3f173ff3ad25da2c2
Christer Edwards <cedwards.edwards@gmail.com>
2010-07-08 13:24:08 -0600

    moved to day2

---
+++ day2/parameters-argv.php
@@ -0,0 +1,13 @@
+<?php
+
+if ($argc <= 2) {
+    print "EPIC FAIL. I need more arguments.\n";
+    return 1;
+}
+
+print "Total arguments: $argc\n";
+print "argv[0] : $argv[0]\n";
+print "argv[1] : $argv[1]\n";
+print "argv[2] : $argv[2]\n";
+
+?>
