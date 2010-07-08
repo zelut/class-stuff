b25364e0deb878ff784786df66b8c46ff22c6a98
Christer Edwards <cedwards.edwards@gmail.com>
2010-07-08 13:23:25 -0600

    moved parameters, recursive to day 2

--- day2/infinite.php
+++ day2/infinite.php
@@ -4,10 +4,7 @@
     // 1 Infinite Loop
     // Cupertino, CA 95014
 
-    // define x
-    $x = 1;
-
-    while ($x == 1) {
+    while (1) {
         print "infinity";
     }
 ?>
--- recursive.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-function factorial($number) {
-      if ($number == 0) return 1;
-      return $number * factorial($number-1);
-}
- 
-print factorial(19) ."\n";
-?>
