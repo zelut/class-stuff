813e6efd7a38a6b5c16c398395226ea21eee6742
Christer Edwards <christer.edwards@gmail.com>
2010-07-27 18:28:31 -0600

    day5 class files

---
+++ day5/mail-mime-class.php
@@ -0,0 +1,20 @@
+<?php
+
+include('Mail.php');
+include('Mail/mime.php');
+ 
+$message = new Mail_mime();
+$text = file_get_contents("pro-git.txt");
+$html = file_get_contents("pro-git.html");
+ 
+$message->setTXTBody($text);
+$message->setHTMLBody($html);
+$message->addAttachment("git-cheat-sheet.php");
+$body = $message->get();
+$extraheaders = array("From"=>"christer.edwards@gmail.com", "Subject"=>"Git Cheat Sheet");
+$headers = $message->headers($extraheaders);
+ 
+$mail = Mail::factory("mail");
+$mail->send("dolson@utahsbr.edu,bhosie@utahsbr.edu", $headers, $body);
+
+?>
